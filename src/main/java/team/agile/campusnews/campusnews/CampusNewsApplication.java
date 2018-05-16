ae8c7bf4c20f560b9d23ce43515dff7e33555f77
董文强 <826112946@qq.com>
2018-05-16 13:37:30 +0800

    新闻系统创建

---
+++ src/main/java/team/agile/campusnews/campusnews/CampusNewsApplication.java
@@ -0,0 +1,12 @@
+package team.agile.campusnews.campusnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusNewsApplication.class, args);
+    }
+}

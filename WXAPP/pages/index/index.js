5cab0c9c09dfaef06311b1dbfe5c3d3fa9c5dc86
董文强 <826112946@qq.com>
2018-05-23 17:36:12 +0800

    修改主页新闻的显示

--- WXAPP/pages/index/index.js
+++ WXAPP/pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     index_index_scroll_tmpl: {
       images: [
         'http://127.0.0.1:8080/file/photo/34088f0a883f4b85ae573a822ff68381.jpg',
-        'http://127.0.0.1:8080/file/photo/34088f0a883f4b85ae573a822ff68381.jpg', 
+        'http://127.0.0.1:8080/file/photo/34088f0a883f4b85ae573a822ff68381.jpg',
         'http://127.0.0.1:8080/file/photo/34088f0a883f4b85ae573a822ff68381.jpg',
         'http://127.0.0.1:8080/file/photo/34088f0a883f4b85ae573a822ff68381.jpg',
         'http://127.0.0.1:8080/file/photo/34088f0a883f4b85ae573a822ff68381.jpg',
@@ -22,36 +22,19 @@ Page({
       interval: 3000,
       duration: 1200
     },
-    // nav
-    index_index_navs_tmpl: {
-      navs: [
-        {
-          image: 'https://m.youcai.xin/static/img/gravida.png',
-          text: '孕妇'
-        }, {
-          image: 'https://m.youcai.xin/static/img/confinement.png',
-          text: '月子'
-        }, {
-          image: 'https://m.youcai.xin/static/img/baby.png',
-          text: '幼儿'
-        }, {
-          image: 'https://m.youcai.xin/static/img/old.png',
-          text: '老人'
-        }
-      ]
-    },
-    // item
-    index_index_items_tmpl: {
-      items: [
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/fe8765fa7f2f48cd87760c10ddd20ae6.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/84439174cad04497beda3a076663beb4.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/1987d8eb8b1748368b7f2382332c9718.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/fe8765fa7f2f48cd87760c10ddd20ae6.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/fe8765fa7f2f48cd87760c10ddd20ae6.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/fe8765fa7f2f48cd87760c10ddd20ae6.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/fe8765fa7f2f48cd87760c10ddd20ae6.jpg' },
-        { image: 'https://hamlet.b0.upaiyun.com/1609/22111/fe8765fa7f2f48cd87760c10ddd20ae6.jpg' }
-      ]
+    
+   //新闻列表内容
+    news_list: {
+      items: [{
+        id: 1,
+        context: "毋庸置疑，Spring Boot在众多.....",
+        title: "我眼中的Spring Boot",
+        pubdate: "2018-05-17T05:28:50.000+0000",
+        author: 1,
+        remark: "转载自CSDN",
+        endTime: "2018-05-25T09:04:58.000+0000",
+        type: "学习"
+      }]
     }
 
   },
@@ -66,40 +49,15 @@ Page({
   }
   ,
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    let that = this;
+    wx.request({
+      url: 'http://127.0.0.1:8080',
+      success: function (res) {
+        that.setData({
+          "news_list.items" : res.data
+        });
       }
-    } else {
-      app.getUserInfo(function (userInfo) {
-        //更新数据
-        console.log("-------------------")
-        console.log(userInfo)
-        if (userInfo) {
-          that.setData({
-            userInfo: userInfo
-          })
-        } else {
-          wx.redirectTo({
-            url: '/reg',
-          })
-        }
-      })
-    }
-
-
-
+    })
   },
   getUserInfo: function (e) {
     console.log(e)

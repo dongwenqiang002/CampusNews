d007c5363c487e867da594ddbd536e2e91303d23
石娟 <18829503014@163.com>
2018-06-05 23:07:38 +0800

    修改数据库以及前端代码

--- WXAPP/pages/list/index.js
+++ WXAPP/pages/list/index.js
@@ -4,18 +4,7 @@ Page({
   data: {
     title: '',
     //新闻列表内容
-    news_list: {
-      items: [{
-        id: 1,
-        context: "毋庸置疑，Spring Boot在众多.....",
-        title: "我眼中的Spring Boot",
-        pubdate: "2018-05-17T05:28:50.000+0000",
-        author: 1,
-        remark: "转载自CSDN",
-        endTime: "2018-05-25T09:04:58.000+0000",
-        type: "学习"
-      }]
-    },
+   
     index_index_navs_tmpl: {
       navs: [
         {
